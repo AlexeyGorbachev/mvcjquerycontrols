c3fcd3054b72703f4216e5bad2c3443212f36335
Alexey Gorbachev <mail_tuser@list.ru>
2017-02-14 13:25:53 +0300

    init https://mvcjquerycontrols.codeplex.com/SourceControl/latest 01_01_2017

---
+++ MVC.Controls.Examples/Models/Store.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Controls.Examples.Models
+{
+    [MetadataType(typeof(MetaData))]
+    public class Store
+    {
+        // To test and demonstrate that MetaData works
+        private static class MetaData
+        {
+            [Display(Name = "My name (FROM META)")]
+            [Required]
+            public static string Name { get; set; }
+        }
+
+        [Key]
+        public int StoreId { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Street Address")]
+        public string Address { get; set; }
+        
+        public List<Product> Products { get; set; }
+        public DateTime Created { get; set; }
+        public Store() { this.Products = new List<Product>(); }
+    }
+}
---
+++ MVC.Controls.Examples/Repositories/StoreRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC.Controls.Examples.Models;
+
+namespace MVC.Controls.Examples.Repositories
+{
+    public class StoreRepository
+    {
+        public List<Store> ListAll()
+        {
+            List<Store> result = new List<Store>();
+            Random r = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Store s = new Store();
+                s.Name = "Store " + i.ToString();
+                s.StoreId = i;
+                s.Address = "Add " + i.ToString();
+                s.Created = new DateTime(2011, r.Next(2, 11), r.Next(5, 20));
+                result.Add(s);
+            }
+
+            return result;
+        }
+
+        public List<Store> GetByProductId(int productId)
+        {
+            List<Store> result = new List<Store>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Store s = new Store();
+                s.Name = "Prod Store " + i.ToString();
+                s.StoreId = i;
+                result.Add(s);
+            }
+
+            return result;
+        }
+    }
+}
